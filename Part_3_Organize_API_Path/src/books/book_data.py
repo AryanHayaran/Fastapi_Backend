09ab5e3fff3da7de851398714c155967b54d77b0
Aryan Hayaran <AryanHayaran>
2025-06-01 22:58:35 +0530

    Part 3 add folder structure

---
+++ Part_3_Organize_API_Path/src/books/book_data.py
@@ -0,0 +1,38 @@
+books = [
+    {
+        "id": 1,
+        "title": "The Pragmatic Programmer",
+        "author": "Andrew Hunt, David Thomas",
+        "publisher": "Addison-Wesley",
+        "published_date": "1999-10-20",
+        "page_count": 352,
+        "language": "en"
+    },
+    {
+        "id": 2,
+        "title": "Clean Code",
+        "author": "Robert C. Martin",
+        "publisher": "Prentice Hall",
+        "published_date": "2008-08-01",
+        "page_count": 464,
+        "language": "en"
+    },
+    {
+        "id": 3,
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen",
+        "publisher": "MIT Press",
+        "published_date": "2009-07-31",
+        "page_count": 1312,
+        "language": "en"
+    },
+    {
+        "id": 4,
+        "title": "Python Crash Course",
+        "author": "Eric Matthes",
+        "publisher": "No Starch Press",
+        "published_date": "2015-11-01",
+        "page_count": 560,
+        "language": "en"
+    }
+]
