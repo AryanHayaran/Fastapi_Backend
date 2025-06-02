961e13d885778400b35fc55d1af28df3f82ce016
Aryan Hayaran <AryanHayaran>
2025-06-03 02:19:15 +0530

    create postgress db

--- Part_3_Organize_API_Path/src/__init__.py
+++ Part_3_Organize_API_Path/src/__init__.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from src.books.routes import book_router
+from src.db.main import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up...")
+    await init_db()
+    yield
+    print("Shutting down...")
 
 version = "v1"
 
 app = FastAPI(
     title="Book API",
     description="A simple API to manage books",
-    version=version)
+    version=version,
+    lifespan=lifespan
+)
 
 app.include_router(book_router, prefix=f"/api/{version}/books")
