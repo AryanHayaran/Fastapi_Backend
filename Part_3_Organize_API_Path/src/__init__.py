c0f4a3e4986aac68312f7a39b32ef0cdfc9a7bac
Aryan Hayaran <AryanHayaran>
2025-06-08 01:59:49 +0530

    add user auth

--- Part_3_Organize_API_Path/src/__init__.py
+++ Part_3_Organize_API_Path/src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.books.routes import book_router
 from src.db.main import init_db
+from src.auth.routes import auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,4 +20,5 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-app.include_router(book_router, prefix=f"/api/{version}/books")
+app.include_router(book_router, prefix=f"/api/{version}/books",tags=["books"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth",tags=["auth"])
--- Part_3_Organize_API_Path/src/auth/models.py
+++ Part_3_Organize_API_Path/src/auth/models.py
@@ -1,11 +1,12 @@
-from sqlmodel import SQLModel,Field,Column
+from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 import uuid
 from datetime import datetime
 
+
 class User(SQLModel, table=True):
     __tablename__ = "users"
-    uid:uuid.UUID= Field(
+    uid: uuid.UUID = Field(
         sa_column=Column(
             pg.UUID,
             nullable=False,
@@ -13,14 +14,16 @@ class User(SQLModel, table=True):
             default=uuid.uuid4(),
         )
     )
-    username:str
-    email:str
-    first_name:str
-    last_name:str
-    is_verified:bool= Field
-    created_at: datetime = Field(sa_column= Column(pg.TIMESTAMP,default=datetime.now))
-    updated_at: datetime = Field(sa_column= Column(pg.TIMESTAMP,default=datetime.now))
-
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(exclude=True)
+    created_at: datetime = Field(sa_column=Column(
+        pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(
+        pg.TIMESTAMP, default=datetime.now))
 
     def __repr__(self):
         return f"<User {self.username}>"
