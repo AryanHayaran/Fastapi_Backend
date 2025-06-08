209e92a3cb940e72be17f660d706ecba5d889756
Aryan Hayaran <AryanHayaran>
2025-06-09 00:39:24 +0530

    refresh token generate access token

--- Part_3_Organize_API_Path/src/auth/routes.py
+++ Part_3_Organize_API_Path/src/auth/routes.py
@@ -5,9 +5,9 @@ from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
 from .utils import create_access_token, decode_token, verify_password
-from datetime import timedelta
+from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
-
+from .dependencies import RefreshTokenBearer
 
 REFRESH_TOKEN_EXPIRY = 2
 
@@ -78,4 +78,17 @@ async def login_users(login_data:UserLoginModel,session: AsyncSession = Depends(
     raise HTTPException(
         status_code=status.HTTP_403_FORBIDDEN,
         detail="Invalid email or password"
+    )
+
+@auth_router.get('/refresh_token')
+async def get_new_access_token(token_details:dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details['exp']
+
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details['user'])
+
+        return JSONResponse(content={"access_token":new_access_token})
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid or expired refresh token"
     )
