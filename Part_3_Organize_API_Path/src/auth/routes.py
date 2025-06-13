d50a5f38dd98a567dee77b0a0a3dbdbdad7f47ba
Aryan Hayaran <AryanHayaran>
2025-06-14 00:23:25 +0530

    add userbooksmodel

--- Part_3_Organize_API_Path/src/auth/routes.py
+++ Part_3_Organize_API_Path/src/auth/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from .service import UserService
-from .schemas import UserCreateModel, UserModel,UserLoginModel
+from .schemas import UserCreateModel, UserModel,UserLoginModel,UserBooksModel
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
@@ -95,7 +95,7 @@ async def get_new_access_token(token_details:dict = Depends(RefreshTokenBearer()
         status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid or expired refresh token"
     )
 
-@auth_router.get("/me",response_model=UserModel)
+@auth_router.get("/me",response_model=UserBooksModel)
 async def get_current_user(user = Depends(get_current_user),_: bool = Depends(role_checker)):
     return user
 
